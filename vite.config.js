c68b8f238226976e30d1957d241226b3f4f79f3f
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2026-03-01 11:09:54 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // relative paths for GitHub Pages
+  base: "/gym-app/", // relative paths for GitHub Pages
 });

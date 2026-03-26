c3beb75d82e7dfb305e8cfa140ca8feca664f19c
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2026-03-26 12:19:38 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/gym-app/", // relative paths for GitHub Pages
+  plugins: [tailwindcss(), react()],
+  base: "/gym-app/",
 });
